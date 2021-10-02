db1c1a8fd3f0719da73a1b14f202249ace212e8c
codergo7 <codergo7@gmail.com>
2021-10-02 10:38:09 +0200

    add practice about variables

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,16 @@
-console.log("Hello Wolrd Learning JS")
-console.log(10+10)
+console.log("Hello Wolrd JS")
+// this is a single line comment
+// this code  prints the result
+console.log(10+10); 
+
+/** This is a multiple line comment 
+ * Semicolon after a statement is optianal
+ * 
+*/
+console.log('You can print me both in double coute and single quote') 
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,14 @@
+var firstName = "Jack";
+var age = 23;
+var isFemale = true;
+var balance = 55.48;
+var dob = new Date(2021,10,2);
+var person = {}
+var empty = undefined
+
+console.log(firstName);
+console.log(age);
+console.log(isFemale); 
+console.log(dob) 
+console.log(person)
+console.log(empty) 
